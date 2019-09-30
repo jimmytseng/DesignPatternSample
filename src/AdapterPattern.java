03b096069cd9633c78a4b7c2bd89508d032d37f2
jimmytseng <jimmytseng@gmail.com>
2019-09-30 21:09:06 +0800

    adapter pattern

---
+++ src/AdapterPattern.java
@@ -0,0 +1,62 @@
+
+public class AdapterPattern {
+
+	public static void main(String[] args) {
+		GooglePayAdaptToApplePay adapter = new GooglePayAdaptToApplePay();
+		MyWalletPay myPay = new MyWalletPay(adapter);
+		myPay.PayMoney();
+	}
+
+}
+
+interface Mypay {
+	void PayMoney();
+}
+
+class MyWalletPay implements Mypay {
+
+	public MyWalletPay(ApplePay applePay) {
+		this.applePay = applePay;
+	}
+
+	ApplePay applePay;
+
+	@Override
+	public void PayMoney() {
+		this.applePay.payAccount();
+	}
+
+}
+
+interface ApplePay {
+	void payAccount();
+}
+
+class ApplePayImpl implements ApplePay {
+	@Override
+	public void payAccount() {
+		System.out.println("this is ApplePay");
+	}
+}
+
+interface GooglePay {
+	void payAccountMoney();
+}
+
+class GooglePayImpl implements GooglePay {
+	@Override
+	public void payAccountMoney() {
+		System.out.println("this is GooglePay");
+	}
+}
+
+class GooglePayAdaptToApplePay implements ApplePay {
+
+	GooglePay googlePay = new GooglePayImpl();
+
+	@Override
+	public void payAccount() {
+		this.googlePay.payAccountMoney();
+	}
+
+}
