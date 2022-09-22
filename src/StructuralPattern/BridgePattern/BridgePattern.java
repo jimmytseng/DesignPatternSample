518e545215f5ee7aa690a94cf368f6a98b1e5850
jimmytseng <jimmytseng@gmail.com>
2022-09-22 16:21:00 +0800

    [BridgePattern]

--- src/StructuralPattern/BridgePattern/BridgePattern.java
+++ src/StructuralPattern/BridgePattern/BridgePattern.java
@@ -4,8 +4,66 @@ package StructuralPattern.BridgePattern;
 public class BridgePattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		IdentifySecurity fingerPrintIdentifySecurity = new FingerPrintIdentifySecurity();
+		PasswordPrintIdentifySecurity passwordPrintIdentifySecurity = new PasswordPrintIdentifySecurity();
+		Phone appleSmartPhone = new AppleSmartPhone(fingerPrintIdentifySecurity);
+		Phone asusSmartPhone = new AsusSmartPhone(passwordPrintIdentifySecurity);
+		appleSmartPhone.checkIdentity();
+		asusSmartPhone.checkIdentity();
 	}
 
+}
+
+interface IdentifySecurity {
+	
+	public void identify();
+}
+
+interface Phone {
+	public void checkIdentity();
+}
+
+class FingerPrintIdentifySecurity implements IdentifySecurity{
+	@Override
+	public void identify() {
+		System.out.print("FingerPrintIdentifySecurity");
+	}
+	
+}
+
+class PasswordPrintIdentifySecurity implements IdentifySecurity{
+	@Override
+	public void identify() {
+		System.out.print("PasswordPrintIdentifySecurity");
+	}
+}
+
+class AppleSmartPhone implements Phone{
+	//bridge
+	private IdentifySecurity identifySecurity;
+	
+	public AppleSmartPhone(IdentifySecurity indentifySecurity) {
+		this.identifySecurity = identifySecurity;
+	}
+
+	@Override
+	public void checkIdentity() {
+		identifySecurity.identify();
+	}
+	
+}
+
+class AsusSmartPhone implements Phone{
+	//bridge
+	private IdentifySecurity identifySecurity;
+	
+	public AsusSmartPhone(IdentifySecurity indentifySecurity) {
+		this.identifySecurity = identifySecurity;
+	}
+	
+	@Override
+	public void checkIdentity() {
+		identifySecurity.identify();
+	}
+	
 }
--- src/StructuralPattern/FacadePattern/README.md
+++ src/StructuralPattern/FacadePattern/README.md
@@ -1,4 +1,4 @@
-FacadePattern隱藏了low-level的實作,可以用最簡單的方式整合物件並
+FacadePattern隱藏了low-level的實作,可以用最簡單的方式整合物件將子系統放入Facade類別中並
 暴露high-level的方法。
 # 使用時機
 1.當你想要隱藏low-level的實作。
