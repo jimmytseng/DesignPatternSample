15f9f5398e5bc52114481bd36b755bcd1da98365
jimmytseng <jimmytseng@gmail.com>
2022-09-22 16:33:50 +0800

    [SingletonPattern] [README.md and class implementation]

--- src/CreationalPattern/SingletonPattern/README.md
+++ src/CreationalPattern/SingletonPattern/README.md
@@ -1,3 +1,4 @@
 # 使用時間
+當你需要整個應用程式只需要有一個實體物件時
 # 如何使用
 ![SingletonPattern](SingletonPattern.jpg)
--- src/CreationalPattern/SingletonPattern/SingletonPattern.java
+++ src/CreationalPattern/SingletonPattern/SingletonPattern.java
@@ -1,7 +1,14 @@
 package CreationalPattern.SingletonPattern;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
 
-public class SingletonPattern {
+public class SingletonPattern implements Serializable {
+
+	public static void main(String[] args) {
+		SingletonPattern sigleton = SingletonPattern.getInstance();
+		System.out.print(sigleton.getName());
+	}
 
 	private String name = "jimmy";
 
@@ -15,6 +22,7 @@ public class SingletonPattern {
 
 	private static SingletonPattern singleton;
 
+	// double locking
 	public static SingletonPattern getInstance() {
 		if (singleton == null) {
 			synchronized (SingletonPattern.class) {
@@ -27,8 +35,22 @@ public class SingletonPattern {
 		return singleton;
 	}
 
-	public static void main(String[] args) {
-		SingletonPattern sigleton = SingletonPattern.getInstance();
-		System.out.print(sigleton.getName());
+	private Object readResolve() throws ObjectStreamException {
+		return singleton;
+	}
+
+	private Object writeReplace() throws ObjectStreamException {
+		return singleton;
+	}
+
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException("cannot be clonned");
+	}
+
+	private static Class getClass(String classname) throws ClassNotFoundException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		if (classLoader == null)
+			classLoader = SingletonPattern.class.getClassLoader();
+		return (classLoader.loadClass(classname));
 	}
 }
